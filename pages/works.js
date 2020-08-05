596d6db04bcbdfc29a16984c78bb019c83e6a9fd
Matthew Lee <matthewlee626@gmail.com>
2020-08-05 00:02:39 -0700

    works.js sass

--- pages/works.js
+++ pages/works.js
@@ -1,8 +1,21 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import {Box} from 'theme-ui'
+import styles from './works.module.sass'
+
+const works = {
+    'items': [
+        {
+            'name': "Octavo",
+            'blurb': "Octavo is cool.",
+            'id': 0
+        }
+    ]
+}
 
 export default function blog() {
+
+
   return (
     <Layout main>
       <Head>
@@ -11,15 +24,23 @@ export default function blog() {
       <section >
         <h2>Works</h2>
         <ul>
-         {'In Progress'}
+         {works['items'].map(({name, blurb, id}) => (
+             <Work
+                id={id}
+                name={name}
+                blurb={blurb}
+             >
+             </Work>
+         ))}
         </ul>
       </section>
     </Layout>
   )
 }
 
-const work = props => (
-    <Box>
-        
+const Work = props => (
+    <Box className={styles.workOverlay}>
+        <p>{props.name}</p>
+        <p>{props.blurb}</p>
     </Box>
 )
