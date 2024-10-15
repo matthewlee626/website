be7f8e2a25a7cdeb16b6c59b2aabdeea07a2af4c
Matthew Lee <matthewlee626@gmail.com>
2024-10-15 04:15:45 -0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function Custom404() {
   return (
-    <div className={`flex flex-cols justify-center items-center w-full p-8 md:h-screen font-sans`}>
+    <div className={`flex flex-cols justify-center items-center w-full p-8 h-screen font-sans`}>
       <p>A dead end!</p>
     </div>
   );
